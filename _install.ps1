b9d97e14705e74fc28b95a5f3bba6f9b4e9774b7
robinmuff <robin.muff@bluewin.ch>
2021-01-27 22:22:08 +0100

    Wrong Link to GIT Repo

--- _install.ps1
+++ _install.ps1
@@ -1,4 +1,4 @@
-New-item –type file –force $profile
+New-item â€“type file â€“force $profile
 cd C:\Users\$env:UserName\Documents\WindowsPowerShell
 Set-Content Microsoft.PowerShell_profile.ps1 ". C:\Users\$env:UserName\Documents\WindowsPowerShell\PowerFunctions\start.ps1"
-git clone https://github.com/robinmuff/M133_Einzelprojekt
+git clone https://github.com/robinmuff/PowerFunctions
